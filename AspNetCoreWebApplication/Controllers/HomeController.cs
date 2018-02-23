48465b86bdb3d5a26d2bfbf289166752b78fa470
jahu99 <jameshulin99@googlemail.com>
2018-02-23 22:53:00 +0000

    Add project files.

---
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "I've changed my first .net application. James you star!";
+            
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["Message"] = "We've encountered an error :(";
+            return View();
+        }
+    }
+}
